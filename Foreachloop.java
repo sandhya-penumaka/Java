c3d980ba3ad11502cce1652a125b43f982d133fc
sandhya-penumaka <151710125+sandhya-penumaka@users.noreply.github.com>
2024-08-17 12:34:25 +0530

    Create Foreachloop.java

---
+++ Foreachloop.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Foreachdemo{
+public static void main(String args[]){
+int arr[]={10,20,30,40,50};
+System.out.println("The array elements are: ");
+for(int i=0;i<5;i++){
+System.out.println(arr[i]);
+}
+}
+}
