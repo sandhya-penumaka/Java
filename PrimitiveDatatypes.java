432147f082c9e7c5044109b53c85a2af2ebd0313
sandhya-penumaka <151710125+sandhya-penumaka@users.noreply.github.com>
2024-11-23 21:01:51 +0530

    Create PrimitiveDatatypes.java

---
+++ PrimitiveDatatypes.java
@@ -0,0 +1,20 @@
+    public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte aByte = 10;
+        short aShort = 30000;
+        int anInt = 100000;
+        long aLong = 100000000L;
+        float aFloat = 5.75f; 
+        double aDouble = 19.99;
+        char aChar = 'A';
+        boolean aBoolean = true; 
+        System.out.println("byte: " + aByte);
+        System.out.println("short: " + aShort);
+        System.out.println("int: " + anInt);
+        System.out.println("long: " + aLong);
+        System.out.println("float: " + aFloat);
+        System.out.println("double: " + aDouble);
+        System.out.println("char: " + aChar);
+        System.out.println("boolean: " + aBoolean);
+    }
+}
